3f9c29a33426a43f417dc8d622679ad0d6dd5f3b
smyhw <i@smyhw.online>
2020-05-10 00:22:52 +0800

    兼容localnet的Json协议

--- src/online/smyhw/localnet/plugins/ClientDataTools/lnp.java
+++ src/online/smyhw/localnet/plugins/ClientDataTools/lnp.java
@@ -34,26 +34,26 @@ public class lnp
 	
 	public static void cmd(Client_sl User,String cmd)
 	{
-		if(User!=LN.local_sl) {User.sendto("抱歉，权限不足");}
+		if(User!=LN.local_sl) {User.sendMsg("抱歉，权限不足");}
 		if(CommandFJ.js(cmd)<4 || CommandFJ.fj(cmd, 1).equals("help"))
 		{
-			if(CommandFJ.js(cmd)<4) {User.sendto("参数不足...");}
-			User.sendto("======ClientDataTools======");
-			User.sendto("指令例如:cdt temp <终端ID> <下列指令> #读取Temp信息");
-			User.sendto("指令例如:cdt bin <下列指令> #读取持久性信息");
-			User.sendto("show #查看目标终端的所有ClientData");
-			User.sendto("del <plugin_ID> <key> #删除指定数据");
-			User.sendto("add <plugin_ID> <key> <value> #添加指定数据");
+			if(CommandFJ.js(cmd)<4) {User.sendMsg("参数不足...");}
+			User.sendMsg("======ClientDataTools======");
+			User.sendMsg("指令例如:cdt temp <终端ID> <下列指令> #读取Temp信息");
+			User.sendMsg("指令例如:cdt bin <下列指令> #读取持久性信息");
+			User.sendMsg("show #查看目标终端的所有ClientData");
+			User.sendMsg("del <plugin_ID> <key> #删除指定数据");
+			User.sendMsg("add <plugin_ID> <key> <value> #添加指定数据");
 			return;
 		}
 		String type = CommandFJ.fj(cmd, 1);
 		data DoMap=null;
 		Client_sl DoClient = LNlib.Find_Client(CommandFJ.fj(cmd, 2));
 		if(DoClient==null) 
-		{User.sendto("终端<"+CommandFJ.fj(cmd, 2)+"不存在");return;}
+		{User.sendMsg("终端<"+CommandFJ.fj(cmd, 2)+"不存在");return;}
 		if(CommandFJ.fj(cmd, 1).equals("bin")) {DoMap = DoClient.ClientData;}
 		if(CommandFJ.fj(cmd, 1).equals("temp")) {DoMap = DoClient.TempClientData;}
-		if(DoMap==null) {User.sendto("类型只能为<bin>或<temp>,而你的输入为<"+CommandFJ.fj(cmd, 1)+">");return;}
+		if(DoMap==null) {User.sendMsg("类型只能为<bin>或<temp>,而你的输入为<"+CommandFJ.fj(cmd, 1)+">");return;}
 		
 		String gg = CommandFJ.fj(cmd, 3);
 		switch(gg)
@@ -61,19 +61,19 @@ public class lnp
 		case "show":
 		{
 			Iterator iter = DoMap.GetClone().entrySet().iterator();
-			User.sendto("终端实例<"+CommandFJ.fj(cmd, 2)+">的ClientData列表");
+			User.sendMsg("终端实例<"+CommandFJ.fj(cmd, 2)+">的ClientData列表");
 			while (iter.hasNext()) 
 			{
 				Map.Entry entry = (Map.Entry) iter.next();
 				String key = (String) entry.getKey();
 				Object val = entry.getValue();
-				User.sendto("KEY=<"+key+">;VALUE=<"+val.toString()+">");
+				User.sendMsg("KEY=<"+key+">;VALUE=<"+val.toString()+">");
 			}
 			break;
 		}
 		case "del":
 		{
-			if(CommandFJ.js(cmd)<6) {User.sendto("参数不足");return;}
+			if(CommandFJ.js(cmd)<6) {User.sendMsg("参数不足");return;}
 			String PluginID = CommandFJ.fj(cmd, 4);
 			String KEY = CommandFJ.fj(cmd, 5);
 			if(CommandFJ.fj(cmd, 1).equals("bin")) 
@@ -84,12 +84,12 @@ public class lnp
 			{
 				DoClient.PutTempClientData(PluginID,KEY,null);
 			}
-			User.sendto("成功将终端<"+CommandFJ.fj(cmd, 2)+">的ClientData<"+PluginID+"><"+KEY+">删除");
+			User.sendMsg("成功将终端<"+CommandFJ.fj(cmd, 2)+">的ClientData<"+PluginID+"><"+KEY+">删除");
 			break;
 		}
 		case "add":
 		{
-			if(CommandFJ.js(cmd)<7) {User.sendto("参数不足");return;}
+			if(CommandFJ.js(cmd)<7) {User.sendMsg("参数不足");return;}
 			String PluginID = CommandFJ.fj(cmd, 4);
 			String KEY = CommandFJ.fj(cmd, 5);
 			String VALUE = CommandFJ.fj(cmd, 6);
@@ -101,12 +101,12 @@ public class lnp
 			{
 				DoClient.PutTempClientData(PluginID,KEY,VALUE);
 			}
-			User.sendto("成功将终端<"+CommandFJ.fj(cmd, 2)+">的ClientData<"+PluginID+"><"+KEY+">设置为<"+VALUE+">");
+			User.sendMsg("成功将终端<"+CommandFJ.fj(cmd, 2)+">的ClientData<"+PluginID+"><"+KEY+">设置为<"+VALUE+">");
 			break;
 		}
 		default:
 		{
-			User.sendto("未知的选项<"+gg+">");
+			User.sendMsg("未知的选项<"+gg+">");
 		}
 		}
 		return;
